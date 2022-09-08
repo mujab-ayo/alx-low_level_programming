d248644ec814d751c7a4bc7fa827b78b6e7763c1
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-08 16:43:44 -0700

    added task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+/**
+ *main - Entry point
+ *
+ * Description: print "Programming is like building a multilingual puzzle using puts function.
+ *
+ * Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
