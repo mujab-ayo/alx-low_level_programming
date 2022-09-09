91a18535f062787d9e9c3937b075394383728803
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-08 17:24:24 -0700

    add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print a sentence
+ *
+ * description: use printf
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return (0);
+}
