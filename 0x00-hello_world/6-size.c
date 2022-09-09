989fb51ab2ce6f53e6e59fe2c5eafddb9d8a83d2
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-08 18:07:55 -0700

    added task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - size of variables
+ *
+ * description: print size of variables
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
+	return (0);
+}
