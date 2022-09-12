818ac1c262e45a104e10534dbc51d69da6ff6bec
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-12 09:03:57 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print single digit number with space and commas inbetween
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
