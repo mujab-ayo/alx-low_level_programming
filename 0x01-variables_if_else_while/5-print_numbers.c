1f8b3594f1be327190df52a90137a816d98dd69f
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-11 20:23:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print number
+ *
+ * Description: print number 1 - 10
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+		putchar(num);
+	return (0);
+}
