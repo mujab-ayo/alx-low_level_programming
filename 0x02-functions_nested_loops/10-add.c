3347cc972f82573d1bbd146b1982de209a04074c
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-14 21:21:07 -0700

    task 10 added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add numbers
+ *
+ * Description: add two numbers;
+ * @num1: integer 1
+ * @num2: integer 2
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
