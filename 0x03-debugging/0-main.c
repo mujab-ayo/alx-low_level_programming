a9984803dd1631524340a2b3e0f8afd7bc4c1f64
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-15 19:28:59 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
-lude "main.h"
+#include "main.h"
 
 
 
 /**
+ * main - tests function that prints if integer is positive or negative
  *
- * * main - tests function that prints if integer is positive or negative
+ * Return: 0
  *
- * * Return: 0
- *
- * */
+ */
 
 
 
@@ -16,9 +15,9 @@ int main(void)
 
 {
 	int i;
-	
+
 	i = 98 % 2;
 	positive_or_negative(i);
-		
+
 	return (0);
 }
