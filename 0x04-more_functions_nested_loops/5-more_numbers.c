5ef4bf3b47179a6fe304c8c6f9cfd5e9a23d0bc0
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-16 20:27:05 -0700

    task 5 added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print number
+ *
+ * Description: print number 0 - 9 in fourteen columns
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 14)
+	{
+		for (j = 0; j <= 9; i++)
+			_putchar(j + '0');
+		_putchar('\n');
+		i++;
+	}
+}
