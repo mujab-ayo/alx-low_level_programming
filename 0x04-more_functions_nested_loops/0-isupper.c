6984180350e5d81a70c6dcca6764f278d1f29e1e
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-16 18:42:54 -0700

    task 1 added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ *
+ * @c: is an integer
+ * Description: check for uppercase alphabet
+ * Return: 1 (if true), else 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
