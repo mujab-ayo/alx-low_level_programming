aadeaff12baa67d34a03db17dbb7c74c9f281c8f
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-16 21:34:11 -0700

    fixing error

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,12 +13,10 @@ void print_most_numbers(void)
 	i = 0;
 	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
+		if (i != 2 && i != 4)
 		{
-			i++;
-			continue;
+			_putchar(i + '0');
 		}
-		_putchar(i + '0');
 		i++;
 	}
 	_putchar('\n');
