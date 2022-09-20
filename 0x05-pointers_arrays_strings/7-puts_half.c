330e1d4e580fba05442e060c2c57d2f0ed50f4f1
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-20 13:31:42 -0700

    task 7 added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print half string
+ *
+ * @str: referenc the string
+ * Description: print half if the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+	int half;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	half = len / 2;
+	for (i = 0; i <= half; i++)
+	{
+		_putchar(str[i];
+	}
+	_putchar('\n');
+}
