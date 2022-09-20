b3a03421ebef6a4089ec6536249cfeec9b07d52f
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-20 16:48:35 -0700

    task 9 added

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; i <= '\0'; i++)
+	for (i = 0; i != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i + 1] = {'\0'};
 	return (dest);
 }
