d00fea35bd8fa0a4bd875120028fb7395b1b1cc8
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-20 16:02:03 -0700

    task 8 added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - print array
+ *
+ * @a: array pointer of int type
+ * @n: is an integer
+ * Description: print n element of an array of integer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n)
+			printf(", ");
+	}
+	printf("\n");
+}
