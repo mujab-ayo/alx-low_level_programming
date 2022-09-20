eb11a2dcee3243ed3f38a4cfb6636169f50809a2
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-19 19:20:09 -0700

    task 1 added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#define "main.h"
+/**
+ * swap_int - swap number
+ *
+ * @a: is an integer
+ * @b: is an integer
+ * Description: swap the value of two numbers
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = *m;
+}
