9992b5b45f905ebaecaa49bffceef1124c0e7047
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-22 20:19:31 -0700

    task 6 added .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+i#include "main.h"
+/**
+ * *cap_string - capitalize string
+ *
+ * @s string to be capitalize
+ * Description: capitalize first letter of every word
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] >= 97 && s[i] <= 122; i++)
+	{
+		if (s[i] == 0 ||
+		    s[i - 1] == ' ' ||
+		    s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == '"' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == '}' ||
+		   )
+		{
+			s[i] -= 32;
+		}
+		return (s);
+	}
+}
