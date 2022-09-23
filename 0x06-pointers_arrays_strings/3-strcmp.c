bfcb079275f6ccb0db4267fa39b80b26cdce0cdc
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-22 18:10:39 -0700

    task 3 added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string
+ *
+ * @s1: string of char type
+ * @s2: string of char type
+ * Description: compare the value two strings
+ * Return: check for the differnce of the first two unmatched character
+ * if s1 > s2  return positive value of the difference
+ * if s1 < s2 return the negative value of the difference
+ * if s1 == s2 return 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			break;
+	}
+
+	return (s1 - s2);
+}
