5bbf2e49dbe5d1c5cb64b5f8cf1883e221aa4e8c
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-22 18:34:16 -0700

    task 3 error fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int diff = 0;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-		return (s1[i] - s2[i]);
+		diff = s1[i] - s2[i];
+
+	return (diff);
 }
