60eb7d5e9d1ed4b2e8e9804a2401628ba9492fcb
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-24 09:09:05 -0700

    checkback

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,26 +9,22 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i < '8'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = i + '1'; j <= '8'; j++)
 		{
-			for (k = '0'; k <= '9'; k++)
+			for (k = j + '1'; j <= '9'; k++)
 			{
-				if (i < j)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-
-					if ((i != '8' || (i == '8' && j != '8')) && k != '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8' && k == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
