03eed8969e920abad56fd4133693ced930e2caa5
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-09-29 09:07:26 -0700

    added task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return the length os string s
+ *
+ * @: string to measure
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	if (*s)
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
+	}
+	return (l);
+}
