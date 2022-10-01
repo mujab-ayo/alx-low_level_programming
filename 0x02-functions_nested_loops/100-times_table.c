4be72715e75c0673a0714672ca2096437c3a9385
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-01 15:04:25 -0700

    attempting difficult task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,12 +37,6 @@ void print_times_table(int n)
 				_putchar((product / 10) + '0');
 
 			_putchar((product % 10) + '0');
-
-			if (column < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 		}
 		_putchar('\n');
 	}
