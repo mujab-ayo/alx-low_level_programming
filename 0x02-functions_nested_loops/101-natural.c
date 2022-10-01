700bd02d890023129fa2b5b43c96779bbbf9db81
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-01 15:18:56 -0700

    task 101 added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/** main - sum of numbers that are multiples of 3
+ * and 5 below 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, sum;
+
+	sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+		printf("%d", sum);
+	}
+	return (0);
+}
