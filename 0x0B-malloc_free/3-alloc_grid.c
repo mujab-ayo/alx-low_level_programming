281e110e6c0cc2012e21c6be1c9b34f638dac23d
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-05 15:40:50 -0700

    task 3 added

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 		if (twoD[hgt_index] == NULL)
 		{
 			for (; hgt_index >= 0; hgt_index--)
-				free(twoD[hgt_index])
+				free(twoD[hgt_index]);
 
 			free(twoD);
 			return (NULL);
