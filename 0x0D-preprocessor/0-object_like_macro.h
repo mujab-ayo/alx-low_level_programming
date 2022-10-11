1fa0aac9bcffb5955b80f0508329d2a110ce7d3e
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-10 19:55:26 -0700

    task o and readme file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,7 +3,7 @@
 #define OBJECT_LIKE_MACRO_H
 /**
  * file: 0-object_like_macro.h
- * Description: a header file that defines a macro named SIZE 
+ * Description: a header file that defines a macro named SIZE
  * as an abbreviation for the token 1024
  */
 #define SIZE 1024
