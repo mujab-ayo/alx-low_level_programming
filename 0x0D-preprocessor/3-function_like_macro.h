d473e200a2b59a52a7dfba9085c2a8fd37eecea8
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-10 20:23:18 -0700

    task 3 & 4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS_H
+
+#define ABS_H
+/**
+ * file: 3-function_like_macro.h
+ * Description: a function-like macro ABS(x) that computes
+ * the absolute value of a number x
+ * @x: number we are finding absolute of
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+
+#define SUM_H
+/**
+ * file: 4-sum.h
+ * Description: a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y
+ * @x: a number
+ * @y: a number
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
