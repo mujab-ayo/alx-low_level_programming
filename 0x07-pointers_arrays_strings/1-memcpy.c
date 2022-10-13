b4e0899298b1e055209e9ffbb60282b9d4e30593
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-13 02:33:33 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory
+ *
+ * @dest: destination memory
+ * @src: source memory
+ * @n: number limit
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
