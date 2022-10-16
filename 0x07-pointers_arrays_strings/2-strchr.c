68036430cd4ef94421ad10188fe7300c6380f252
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2022-10-16 02:35:19 -0700

    task 2 added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;	     
-     	while (s[i] != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (s[i] == c)			
-			return (s);				
-		i++;		
-	}	
-	return('\0');
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return ('\0');
 }
